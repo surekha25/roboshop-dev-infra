1257d4c0ced61606c9aebfac3270ef1d1817bc0e
sivakumar reddy <info@joindevops.com>
2025-10-25 08:47:17 +0530

    terraform

--- 10-sg/main.tf
+++ 10-sg/main.tf
@@ -1,4 +1,5 @@
-module "catalogue" {
+# Using Open source module
+/* module "catalogue" {
   source = "terraform-aws-modules/security-group/aws"
 
   name        = "${local.common_name_suffix}-catalogue"
@@ -6,4 +7,15 @@ module "catalogue" {
   description = "Security group for catalogue with custom ports open within VPC, egress all traffic"
   vpc_id      = data.aws_ssm_parameter.vpc_id.value
 
+} */
+
+
+module "sg" {
+  count = length(var.sg_names)
+  source = "git::https://github.com/daws-86s/terraform-aws-sg.git?ref=main"
+  project_name = var.project_name
+  environment = var.environment
+  sg_name = var.sg_names[count.index]
+  sg_description = "Created for ${var.sg_names[count.index]}"
+  vpc_id =  local.vpc_id
 }
