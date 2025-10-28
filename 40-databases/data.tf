14c6be3d30cbb869b0184f0616d0f83e471a20d5
sivakumar reddy <info@joindevops.com>
2025-10-28 08:37:11 +0530

    terraform

--- 40-databases/data.tf
+++ 40-databases/data.tf
@@ -28,6 +28,15 @@ data "aws_ssm_parameter" "redis_sg_id" {
   name = "/${var.project_name}/${var.environment}/redis_sg_id"
 }
 
+data "aws_ssm_parameter" "rabbitmq_sg_id" {
+  name = "/${var.project_name}/${var.environment}/rabbitmq_sg_id"
+}
+
+data "aws_ssm_parameter" "mysql_sg_id" {
+  name = "/${var.project_name}/${var.environment}/mysql_sg_id"
+}
+
+
 data "aws_ssm_parameter" "database_subnet_ids" {
   name = "/${var.project_name}/${var.environment}/database_subnet_ids"
 }
