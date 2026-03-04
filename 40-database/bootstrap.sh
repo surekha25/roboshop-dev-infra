e849894d9ca8e5febce0865861ba4ef33e9d6771
surekha25 <surekhasuri25@gmail.com>
2026-03-04 21:21:41 +0530

    terraform

--- 40-database/bootstrap.sh
+++ 40-database/bootstrap.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+
+component=$1
 dnf install ansible -y
-ansible-pull -U https://github.com/surekha25/ansible-roboshop-roles-tf.git -e component=mongodb main.yaml
+ansible-pull -U https://github.com/surekha25/ansible-roboshop-roles-tf.git -e component=$component main.yaml
--- 40-database/locals.tf
+++ 40-database/locals.tf
@@ -1,6 +1,7 @@
 locals {
   ami_id = data.aws_ami.joindevops.id
-  mongodb_sg_id = data.aws_ssm_parameter.mongodb_sg_id.value  
+  mongodb_sg_id = data.aws_ssm_parameter.mongodb_sg_id.value
+  redis_sg_id = data.aws_ssm_parameter.redis_sg_id.value  
   database_subnet_id = split("," , data.aws_ssm_parameter.database_subnet_ids.value)[0]
   common_name_suffix = "${var.project_name}-${var.environment}" #roboshop-dev
   common_tags = {
