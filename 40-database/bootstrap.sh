7eaff0910a0aea8f4f66c11e06d26bc4c12f6b06
surekha25 <surekhasuri25@gmail.com>
2026-03-05 18:41:08 +0530

    terraform

--- 40-database/bootstrap.sh
+++ 40-database/bootstrap.sh
@@ -25,4 +25,4 @@ else
     cd $ANSIBLE_DIR
 fi
 
-ansible-playbook -e component=$component environment=$environment main.yaml
+ansible-playbook -e "component=$component environment=$environment" main.yaml
