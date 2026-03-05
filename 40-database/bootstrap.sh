f50baa0da833febef5ce270aab5c86fd8833b2b5
surekha25 <surekhasuri25@gmail.com>
2026-03-05 17:44:33 +0530

    terraform

--- 40-database/bootstrap.sh
+++ 40-database/bootstrap.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 component=$1
+env=$2  
 dnf install ansible -y
 # ansible-pull -U https://github.com/surekha25/ansible-roboshop-roles-tf.git -e component=$component main.yaml
 
@@ -24,4 +25,4 @@ else
     cd $ANSIBLE_DIR
 fi
 
-ansible-playbook -e component=$component main.yaml
+ansible-playbook -e component=$component env=$env main.yaml
--- 40-database/main.tf
+++ 40-database/main.tf
@@ -118,43 +118,49 @@ resource "terraform_data" "rabbitmq" {
   }
 }
 
-# resource "aws_instance" "mysql" {
-#   ami           = local.ami_id
-#   instance_type = "t3.micro"
-#   vpc_security_group_ids = [local.mysql_sg_id]
-#   subnet_id = local.database_subnet_id
-  
-#   tags = merge(
-#     local.common_tags,
-#     {
-#         Name = "${local.common_name_suffix}-mysql"
-#     }
-#   )
-# }
+resource "aws_instance" "mysql" {
+  ami           = local.ami_id
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [local.mysql_sg_id]
+  subnet_id = local.database_subnet_id
+  iam_instance_profile = aws_iam_instance_profile.mysql.name
 
-# resource "terraform_data" "mysql" {
-#   triggers_replace = [
-#     aws_instance.mysql.id
-#   ]
+  tags = merge(
+    local.common_tags,
+    {
+        Name = "${local.common_name_suffix}-mysql"
+    }
+  )
+}
 
-#   connection {
-#     type     = "ssh"
-#     user     = "ec2-user"
-#     password = "DevOps321"
-#     host     = aws_instance.mysql.private_ip
-#   }
+resource "aws_iam_instance_profile" "mysql" {
+  name = "mysql"
+  role = "Ec2ssmparameterRead"  
+}
 
-#   # terraform copies this file to mysql server
-#   provisioner "file" {
-#     source = "bootstrap.sh"
-#     destination = "/tmp/bootstrap.sh"
-#   }
+resource "terraform_data" "mysql" {
+  triggers_replace = [
+    aws_instance.mysql.id
+  ]
 
-#   provisioner "remote-exec" {
-#     inline = [ 
-#         "chmod +x /tmp/bootstrap.sh",
-#         "sudo sh /tmp/bootstrap.sh mysql"
-#      ]
-#   }
-# }
+  connection {
+    type     = "ssh"
+    user     = "ec2-user"
+    password = "DevOps321"
+    host     = aws_instance.mysql.private_ip
+  }
+
+  # terraform copies this file to mysql server
+  provisioner "file" {
+    source = "bootstrap.sh"
+    destination = "/tmp/bootstrap.sh"
+  }
+
+  provisioner "remote-exec" {
+    inline = [ 
+        "chmod +x /tmp/bootstrap.sh",
+        "sudo sh /tmp/bootstrap.sh mysql dev"
+     ]
+  }
+}
 
