775259cca16d6993b73ba461ac0bd731ac19c3cf
surekha25 <surekhasuri25@gmail.com>
2026-03-05 18:10:24 +0530

    terraform

--- 40-database/bootstrap.sh
+++ 40-database/bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 component=$1
-env=$2  
+environment=$2  
 dnf install ansible -y
 # ansible-pull -U https://github.com/surekha25/ansible-roboshop-roles-tf.git -e component=$component main.yaml
 
@@ -25,4 +25,4 @@ else
     cd $ANSIBLE_DIR
 fi
 
-ansible-playbook -e component=$component env=$env main.yaml
+ansible-playbook -e component=$component environment=$environment main.yaml
