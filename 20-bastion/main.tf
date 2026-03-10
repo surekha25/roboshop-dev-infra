b46cf0c9c1cd14297df70529a56490b4751c0c40
surekha25 <surekhasuri25@gmail.com>
2026-03-10 12:59:00 +0530

    terraform

--- 20-bastion/main.tf
+++ 20-bastion/main.tf
@@ -3,6 +3,7 @@ resource "aws_instance" "bastion" {
   instance_type = "t3.micro"
   vpc_security_group_ids = [local.bastion_sg_id]
   subnet_id = local.public_subnet_id
+  iam_instance_profile = aws_iam_instance_profile.bastion.name
   
   user_data = file("bastion.sh")
   tags = merge(
@@ -11,4 +12,9 @@ resource "aws_instance" "bastion" {
         Name = "${local.common_name_suffix}-bastion"
     }
   )
+}
+
+resource "aws_iam_instance_profile" "bastion" {
+  name = "bastion"
+  role = "bastion-terraform-admin"  
 }
