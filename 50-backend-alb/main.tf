d0af8c985e90700510face3e58181150a7e4b5f8
surekha25 <surekhasuri25@gmail.com>
2026-03-12 20:23:56 +0530

    terraform

--- 50-backend-alb/main.tf
+++ 50-backend-alb/main.tf
@@ -51,7 +51,7 @@ resource "aws_lb_listener" "backend_alb" {
 
 resource "aws_route53_record" "backend_alb" {
   zone_id = var.zone_id
-  name    = "*.backned-alb-${var.environment}.${var.domain}"
+  name    = "*.backend-alb-${var.environment}.${var.domain}"
   type    = "A"
 
   alias {
