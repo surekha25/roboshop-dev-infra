746cba4f212b51d577d66e9ebf7725b18990ba59
surekha25 <surekhasuri25@gmail.com>
2026-03-12 19:56:22 +0530

    terraform

--- 50-backend-alb/main.tf
+++ 50-backend-alb/main.tf
@@ -32,6 +32,23 @@ resource "aws_lb_listener" "front_end" {
   }
 }
 
+# Backend ALB Listener on Port Number 80 
+resource "aws_lb_listener" "backend_alb" {
+  load_balancer_arn = aws_lb.backend_alb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "Hi, I am from Backend ALB HTTP" 
+      status_code  = "200"
+    }
+  }
+}
+
 resource "aws_route53_record" "backend_alb" {
   zone_id = var.zone_id
   name    = "*.backned-alb-${var.environment}.${var.domain}"
