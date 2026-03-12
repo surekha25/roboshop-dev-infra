6c3a1781f7b00caccc20984aa533f4a2e36cf88f
surekha25 <surekhasuri25@gmail.com>
2026-03-12 20:15:27 +0530

    terraform

--- 60-catalogue/locals.tf
+++ 60-catalogue/locals.tf
@@ -3,6 +3,7 @@ locals {
   catalogue_sg_id = data.aws_ssm_parameter.catalogue_sg_id.value
   vpc_id = data.aws_ssm_parameter.vpc_id.value  
   private_subnet_id = split("," , data.aws_ssm_parameter.private_subnet_ids.value)[0]
+  backend_alb_listener_arn = data.aws_ssm_parameter.backend_alb_listener_arn.value
   common_name_suffix = "${var.project_name}-${var.environment}" #roboshop-dev
   common_tags = {
     Project = var.project_name
