7e31a8c93045b1c5297475f558ace40682941a3b
surekha25 <surekhasuri25@gmail.com>
2026-03-12 19:30:38 +0530

    terraform

--- 50-backend-alb/main.tf
+++ 50-backend-alb/main.tf
@@ -31,3 +31,16 @@ resource "aws_lb_listener" "front_end" {
     }
   }
 }
+
+resource "aws_route53_record" "backend_alb" {
+  zone_id = var.zone_id
+  name    = "*.backned-alb-${var.environment}.${var.domain}"
+  type    = "A"
+
+  alias {
+    # These are ALB details, not our domain details
+    name                   = aws_elb.backend_alb.dns_name
+    zone_id                = aws_elb.backend_alb.zone_id
+    evaluate_target_health = true
+  }
+}
--- 50-backend-alb/variables.tf
+++ 50-backend-alb/variables.tf
@@ -5,3 +5,11 @@ variable "project_name" {
 variable "environment" {
     default = "dev"
 }
+
+variable "zone_id"{
+    default = "Z03322182ZV3F5YEDZYG5"
+}
+
+variable "domain"{
+    default = "surekhadevops.biz"
+}
