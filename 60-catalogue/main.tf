fecc19705cce3210a5081a89e13224a59ca77cfd
surekha25 <surekhasuri25@gmail.com>
2026-03-13 12:53:01 +0530

    terraform

--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -91,6 +91,9 @@ resource "aws_launch_template" "catalogue" {
 
   vpc_security_group_ids = [local.catalogue_sg_id]
 
+  # when we run terrafrom apply again, a new version is created with new AMI ID
+  update_default_version = true
+
   # tags attached to the instance
   tag_specifications {
     resource_type = "instance"
@@ -138,6 +141,14 @@ resource "aws_autoscaling_group" "catalogue" {
   }
   vpc_zone_identifier       = [ local.private_subnet_id ]
   target_group_arns = [ aws_lb_target_group.catalogue.arn ]
+
+  instance_refresh {
+    strategy = "Rolling"
+    preferences {
+      min_healthy_percentage = 50 # atleast 50% of the instances should be up and running
+    }
+    triggers = ["launch_template"]
+  }
 
   dynamic "tag" { # we will get the iterator name as tag
     for_each = merge(
