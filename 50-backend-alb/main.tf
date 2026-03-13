0e4022ee08a67f3eead6a69756931a78e516c560
surekha25 <surekhasuri25@gmail.com>
2026-03-13 10:49:09 +0530

    terraform

--- 50-backend-alb/main.tf
+++ 50-backend-alb/main.tf
@@ -5,7 +5,7 @@ resource "aws_lb" "backend_alb" {
   security_groups    = [local.backend_alb_sg_id]
   subnets            = local.private_subnet_ids
 
-  enable_deletion_protection = true # prevents accidental deletion from UI
+  enable_deletion_protection = false # prevents accidental deletion from UI
 
   tags = merge(
     local.common_tags,
