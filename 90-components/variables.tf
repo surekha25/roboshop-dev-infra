8e1c36c5cad602bd041bb8fb8d64b67b5cf290c4
surekha25 <surekhasuri25@gmail.com>
2026-03-18 19:23:32 +0530

    terraform

--- 90-components/variables.tf
+++ 90-components/variables.tf
@@ -7,24 +7,16 @@
 # }
 
 variable "components" {
+  type = map(object({
+    rule_priority = number
+  }))
+
   default = {
-    catalogue = {
-      rule_priority = 10
-    }
-    user = {
-      rule_priority = 20
-    }
-    cart = {
-      rule_priority = 30
-    }
-    shipping = {
-      rule_priority = 40
-    }
-    payment = {
-      rule_priority = 50
-    }
-    frontend = {
-      rule_priority = 60
-    }
+    catalogue = { rule_priority = 10 }
+    user      = { rule_priority = 20 }
+    cart      = { rule_priority = 30 }
+    shipping  = { rule_priority = 40 }
+    payment   = { rule_priority = 50 }
+    frontend  = { rule_priority = 60 }
   }
 }
